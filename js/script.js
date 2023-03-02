e6360d218b81faa34539a4f790cc99c08d02a945
gabriel <gabriel.f.lazari@gmail.com>
2023-03-02 10:28:56 -0300

    api

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-const consultaCEP = fetch('viacep.com.br/ws/01001000/json/');
+const consultaCEP = fetch('https://viacep.com.br/ws/08030760/json/')
+.then(resposta => resposta.json())
+.then(r => {
+    if(r.erro) {
+        throw Error('Esse cep não existe!');
+    } else {
+        console.log(r);
+    }
+})
+.catch(erro => console.log(erro))
+.finally(mensagem => console.log('Processamento concluido!'));
+
 console.log(consultaCEP)
