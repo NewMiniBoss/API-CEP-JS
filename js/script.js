49b94fbf0902dfa65e7fbd59e2551c16b8545ee2
gabriel <gabriel.f.lazari@gmail.com>
2023-03-02 15:12:58 -0300

    seguranca/organizando

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-const consultaCEP = fetch('https://viacep.com.br/ws/08030760/json/')
-.then(resposta => resposta.json())
-.then(r => {
-    if(r.erro) {
-        throw Error('Esse cep não existe!');
-    } else {
-        console.log(r);
-    }
-})
-.catch(erro => console.log(erro))
-.finally(mensagem => console.log('Processamento concluido!'));
+(() => {
+    const consultaCEP = fetch('https://viacep.com.br/ws/08030760/json/')
+        .then(resposta => resposta.json())
+        .then(r => {
+            if (r.erro) {
+                throw Error('Esse cep não existe!');
+            } else {
+                console.log(r);
+            }
+        })
+        .catch(erro => console.log(erro))
+        .finally(mensagem => console.log('Processamento concluido!'));
 
-console.log(consultaCEP)
+    console.log(consultaCEP)
+})();
